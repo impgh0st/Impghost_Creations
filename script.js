22d21778c90d7cad0609d29ca6a20980bc660098
DJ <impGHOST@live.com>
2023-11-25 12:53:35 -0700

    full site update 11/25/2023

--- script.js
+++ script.js
@@ -28,35 +28,3 @@ nextBtn.addEventListener('click', () => {
 
 // Show the initial project
 showProject(currentIndex);
-
-// script.js
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the design div
-    var designDiv = document.querySelector('.design');
-  
-    // Get the paragraph element within the design div
-    var paragraph = designDiv.querySelector('p');
-  
-    // Save the default text content
-    var defaultText = paragraph.textContent;
-  
-    // Add mouseover and mouseout event listeners to the design div
-    designDiv.addEventListener('mouseover', function () {
-      // Add the 'active' class on hover
-      this.classList.add('active');
-  
-      // Change the text content to the custom message
-      paragraph.textContent = "Web design is the combination of aesthetics and presentation for the user.";
-    });
-  
-    designDiv.addEventListener('mouseout', function () {
-      // Remove the 'active' class on mouseout
-      this.classList.remove('active');
-  
-      // Restore the default text content
-      paragraph.textContent = defaultText;
-    });
-  });
-  
-  
